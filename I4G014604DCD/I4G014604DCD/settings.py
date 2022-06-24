a035b21dab3a0aa9c5294ea235e4d6dba8409792
Gift Balogun <blgnbalogun53@gmail.com>
2022-06-24 19:55:13 +0100

    Django CRUD

--- I4G014604DCD/I4G014604DCD/settings.py
+++ I4G014604DCD/I4G014604DCD/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,10 +53,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'I4G014604DCD.urls'
 
+TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
